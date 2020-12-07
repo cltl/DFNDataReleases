236ec164874088adbed29b6a9b57484a1db50cce
MartenPostma <martenp@gmail.com>
2020-12-07 15:43:29 +0100

    annotation session data release

--- install.sh
+++ install.sh
@@ -1,31 +1,33 @@
 rm -rf res
 mkdir res
 cd res || exit
-git clone https://github.com/cltl/multilingual-wiki-event-pipeline
+git clone https://github.com/cltl/multilingual-wiki-event-pipeline || exit
 
 rm -rf dfn
 mkdir dfn
 cd dfn || exit
-wget https://github.com/cltl/DutchFrameNet/archive/v0.1.zip
+wget https://github.com/cltl/DutchFrameNet/archive/v0.1.zip || exit
 unzip v0.1.zip
 cd ../..
 
 cd src || exit
 rm -rf LexicalDataD2TAnnotationTool
-git clone https://github.com/cltl/LexicalDataD2TAnnotationTool
+git clone https://github.com/cltl/LexicalDataD2TAnnotationTool || exit
 cd LexicalDataD2TAnnotationTool || exit
 pip install -r requirements.txt
 bash install.sh
 cd ../..
 
 cd src || exit
-git clone https://github.com/cltl/FrameNetNLTK
+rm -rf FrameNetNLTK
+git clone https://github.com/cltl/FrameNetNLTK || exit
 cd FrameNetNLTK || exit
 pip install -r requirements.txt
 bash install.sh
 cd ..
 
-git clone https://github.com/cltl/historical_distance
+rm -rf historical_distance || exit
+git clone https://github.com/cltl/historical_distance || exit
 
 
 
